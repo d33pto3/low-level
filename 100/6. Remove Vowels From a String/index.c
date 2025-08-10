e8074ae4c4fb0201c5fffbd26527bd67758c7b1c
d33pto3 <rakinfrhn@gmail.com>
2025-08-10 14:04:57 +0600

    Remove vowel from string

---
+++ 100/6. Remove Vowels From a String/index.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void remove_vowels(char *str)
+{
+    if (*str == '\0')
+        return;
+
+    if (*str == 'a' || *str == 'e' || *str == 'i' || *str == 'o' || *str == 'u')
+    {
+        int i = 0;
+        while (str[i] != '\0')
+        {
+            str[i] = str[i + 1];
+            i++;
+        }
+        remove_vowels(str);
+    }
+    else
+        remove_vowels(++str);
+}
+
+void printt(char *str)
+{
+    while (*str != '\0')
+    {
+        printf("%c", *str);
+        ++str;
+    }
+}
+
+int main()
+{
+    // code
+    char str[] = "ovwel";
+
+    remove_vowels(str);
+
+    printt(str);
+
+    return 0;
+}
