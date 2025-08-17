ff6f5b6e2acfda46ecee6edf082731a2b7c2a820
d33pto3 <rakinfrhn@gmail.com>
2025-08-17 14:24:39 +0600

    13

--- 100/13. Longest Common Prefix in Array of Strings/main.c
+++ 100/13. Longest Common Prefix in Array of Strings/main.c
@@ -1,18 +1,48 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+
+char *longestCommonPrefix(char **strs, int strsSize)
+{
+    if (strsSize == 0)
+        return "";
+
+    char *prefix = strs[0]; // take the first string as reference
+    int prefixLen = strlen(prefix);
+
+    for (int i = 1; i < strsSize; i++)
+    {
+        char *cur = strs[i];
+
+        int j = 0;
+
+        // Compare characters using pointers
+        while (j < prefixLen && *(prefix + j) && *(cur + j) && *(prefix + j) == *(cur + j))
+        {
+            j++;
+        }
+
+        // shrink the prefix length
+        prefixLen = j;
+
+        if (prefixLen == 0)
+            return "";
+
+        *(prefix + prefixLen) = '\0';
+        return prefix;
+    }
+}
+
 int main()
 {
-    int count = 3;   // number of strings
-    int length = 20; // max length of each string
+    char str1[] = "flower";
+    char str2[] = "flight";
+    char str3[] = "flow";
 
-    // Allocate array of char* (string pointers)
-    char **arr = malloc(count * sizeof(char *));
+    char *arr[] = {str1, str2, str3};
+    int size = sizeof(arr) / sizeof(arr[0]);
 
-    // Allocate space for each string
-    for (int i = 0; i < count; i++)
-    {
-        arr[i] = malloc(length * sizeof(char *));
-    }
+    char *result = longestCommonPrefix(arr, size);
+    printf("Longest common prefix: %s\n", result);
 
     return 0;
 }
