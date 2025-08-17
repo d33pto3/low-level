5ae0ada48b3e38df17e39cc704383dd0b43e304f
d33pto3 <rakinfrhn@gmail.com>
2025-08-18 00:41:41 +0600

    Here we 14 again

--- 100/14. Check if Anagram/main.c
+++ 100/14. Check if Anagram/main.c
@@ -1,60 +1,20 @@
 #include <stdio.h>
-#include <string.h>
+
+int anagram_cf(char *, char *);
+
 int main()
 {
-    char str1[] = "flow";
-    char str2[] = "wolv";
+    // code
+    char *str1;
+    char *str2;
 
-    int anagram = 1;
+    scanf("%s", str1);
+    scanf("%s", str2);
 
-    if (strlen(str1) != strlen(str2))
-    {
-        printf("Not Anagram!");
-        return 0;
-    }
+    if (anagram_cf(str1, str2))
+        printf("Anagram\n");
     else
-    {
-        int len = strlen(str1);
-
-        int pos[len];
-
-        for (int i = 0; i < len; i++)
-        {
-            pos[i] = 0;
-        }
-
-        for (int i = 0; i < len; i++)
-        {
-            char cur = str1[i];
-
-            for (int j = 0; j < len; j++)
-            {
-                if (!pos[j] && cur == str2[j])
-                {
-                    // printf("%d - %c\n", j, str2[j]);
-                    pos[j] = 1;
-                    break;
-                }
-
-                if (j == len - 1)
-                {
-                    anagram = 0;
-                }
-            }
-
-            if (!anagram)
-                break;
-        }
-    }
-
-    if (!anagram)
-    {
-        printf("Not Anagram!");
-    }
-    else
-    {
-        printf("Anagram!");
-    }
+        printf("Not Anagram\n");
 
     return 0;
 }
