8faf5bde72355aa3f55dfe2afc3ba1284bc40865
d33pto3 <rakinfrhn@gmail.com>
2025-08-23 16:05:07 +0600

    Simulate strcmp using pointer arithmetic

--- 100/18. Simulate strcmp Using Pointer Arithmetic/main.c
+++ 100/18. Simulate strcmp Using Pointer Arithmetic/main.c
@@ -3,24 +3,26 @@
 
 int string_compare(char *s1, char *s2)
 {
-    while (*s1 != '\0' || *s2 != '\0')
+    while (*s1 != '\0' && *s2 != '\0')
     {
-        printf("%c %p ", *s1, s1);
-        printf("%c %p\n", *s2, s2);
-
+        if (*s1 != *s2)
+        {
+            return (*s1 - *s2);
+        }
         s1++;
         s2++;
     }
-    return 0;
+
+    return *s1 - *s2;
 }
 
 int main()
 {
-    char *str1 = "Hello";
+    char *str1 = "He";
     char *str2 = "Hel";
 
-    printf("\n\n%d\n", string_compare(str1, str2));
+    printf("%d\n", string_compare(str1, str2));
 
-    // printf("%d\n", strcmp(str1, str2));
+    printf("%d\n", strcmp(str1, str2));
     return 0;
 }
