9bfb10499e0633d14df327607814a4894582a6cb
d33pto3 <rakinfrhn@gmail.com>
2025-09-14 11:33:13 +0600

    implement readv

---
+++ io/impl_readv/main.c
@@ -0,0 +1,77 @@
+// AI Generated
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+// Define iovec for portability
+struct iovec
+{
+    void *iov_base; // buffer start
+    size_t iov_len;
+};
+
+ssize_t my_readv(int fd, const struct iovec *iov, int iovcnt)
+{
+    ssize_t total_read = 0;
+
+    for (int i = 0; i < iovcnt; i++)
+    {
+        size_t bytes_left = iov[i].iov_len;
+        char *buffer = (char *)iov[i].iov_base;
+
+        while (bytes_left > 0)
+        {
+            ssize_t n = read(fd, buffer, bytes_left);
+            if (n == 0)
+            {
+                // EOF
+                return total_read;
+            }
+            if (n == -1)
+            {
+                return (total_read > 0) ? total_read : -1;
+            }
+
+            buffer += n;
+            bytes_left -= n;
+            total_read += n;
+        }
+    }
+
+    return total_read;
+}
+
+int main()
+{
+    struct iovec iov[2];
+
+    char *buf1 = malloc(10);
+    char *buf2 = malloc(20);
+
+    iov[0].iov_base = buf1;
+    iov[0].iov_len = 10;
+
+    iov[1].iov_base = buf2;
+    iov[1].iov_len = 20;
+
+    int fd = STDIN_FILENO; // read from standard input
+
+    ssize_t n = my_readv(fd, iov, 2);
+
+    if (n == -1)
+    {
+        perror("my_readv");
+        return 1;
+    }
+
+    printf("Read %zd bytes\n", n);
+    printf("Buffer 1: %.*s\n", 10, buf1);
+    printf("Buffer 2: %.*s\n", 20, buf2);
+
+    free(buf1);
+    free(buf2);
+
+    return 0;
+}
